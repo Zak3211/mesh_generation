14772314740d8e7ddb4e579b7270c0ba06d18d53
Zak321171 <zakzakono@gmail.com>
2026-04-27 10:33:40 +0100

    Matching both branches

--- boundary.py
+++ boundary.py
@@ -5,15 +5,14 @@ class boundary:
     
     def __init__(self, edges : list[edge]):
         self.edges = edges
-
         self.node_set = self.get_node_set() # Set containing all active node coordinates
 
-        self.int_coords_to_node = defaultdict(set) # (int(x), int(y)) -> set((x,y)), all nodes within an integer quadrant
-        self.coordinates_to_node = {} # (x, y) -> unique node
-        self.populate_parameter_dictionaries()
+        # (int(x), int(y)) -> set((x,y)), all nodes within an integer quadrant
+        self.int_coords_to_node = defaultdict(set)
 
-    def populate_parameter_dictionaries(self):
-        """Populates self.int_coordinates_to_node, and self.coordinates_to_node"""
+        # (x, y) -> unique node
+        self.coordinates_to_node = {} 
+
         for node in self.node_set:
             int_coordinates = node.get_integer_coordinates()
             self.int_coords_to_node[int_coordinates].add(coordinates)
@@ -27,7 +26,7 @@ class boundary:
             node_set.update(edge.get_coordinates())
         return node_set
 
-    def find_nearby_nodes(self, node, maximum_distance):
+    def find_nearby_nodes(self, node):
         """Returns a set of neighbor candidates"""
         int_x, int_y = node.get_integer_coordinates()
         neighbor_candidates = set()
@@ -36,8 +35,6 @@ class boundary:
                 curr_neighbors = self.int_coords_to_node[(x,y)]
                 neighbor_candidates.update(curr_neighbors)
         return neighbor_candidates
-
-
     
     def expand_edge(self):
         pass
